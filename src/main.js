6989d06c76b0ab695f88811bc3a3e90f96b14d23
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-12-24 13:08:50 +0200

    text

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import gsap from "gsap";
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+
+// tl.from(".samsung_logo", { opacity: 0, duration: 1, delay: 1, y: 100, x: -100, ease: "power2.out" });
